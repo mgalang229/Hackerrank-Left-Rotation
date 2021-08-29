edba618fefe5f3e284e2b81a13a4f14ee09b06ad
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-29 17:29:13 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,23 +1,30 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
+using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int n, d;
 	cin >> n >> d;
-	int a[n];
-	for(int i=0; i<n; ++i)
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
 		cin >> a[i];
-	vector<int> ans;
-	for(int i=d%n; i<n; ++i)
-		ans.push_back(a[i]);
-	for(int i=0; i<d%n; ++i)
-		ans.push_back(a[i]);
-	for(auto x : ans)
+	}
+	vector<int> res;
+	// elements starting from the ('d' mod 'n')-th position 
+	// up to the last position will be rotated to the starting position
+	for (int i = d % n; i < n; i++) {
+		res.push_back(a[i]);
+	}
+	// elements starting from the 0 up to the ('d' mod 'n') - 1-th position 
+	// will have their positions switched with the elements above
+	for (int i = 0; i < d % n; i++) {
+		res.push_back(a[i]);
+	}
+	for (auto x : res) {
 		cout << x << " ";
-	cout << "\n";
+	}
+	cout << '\n';
+	return 0;
 }
